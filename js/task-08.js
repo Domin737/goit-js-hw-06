d70268467a4e693b606922ead2fec3b5e2dc7417
Domin737 <p.dominiak.pd@gmail.com>
2023-07-25 00:40:09 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (event) => {
+  // Zapobiegaj domyślnej akcji formularza
+  event.preventDefault();
+
+  const { email, password } = event.target.elements;
+
+  // Sprawdź, czy oba pola są wypełnione
+  if (!email.value || !password.value) {
+    alert("Wszystkie pola powinny zostać wypełnione!");
+    return;
+  }
+
+  // Utwórz obiekt z wartościami formularza
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  // Wypisz obiekt w konsoli
+  console.log(formData);
+
+  // Wyczyść formularz
+  event.target.reset();
+});
