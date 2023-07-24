cbf3ec0abc612bc4e27f656a9c24a59c26b12baa
Domin737 <p.dominiak.pd@gmail.com>
2023-07-25 00:42:41 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorButton = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+
+changeColorButton.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+
+  // Zmień kolor tła elementu <body>
+  document.body.style.backgroundColor = randomColor;
+
+  // Ustaw tekst elementu span.color na wartość wygenerowanego koloru
+  colorSpan.textContent = randomColor;
+});
