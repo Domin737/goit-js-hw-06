ab6c3b865507d0116b2eca70bb97a1f019576245
Domin737 <p.dominiak.pd@gmail.com>
2023-07-24 23:41:55 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+// Utwórz tablicę elementów li na podstawie tablicy ingredients
+const elements = ingredients.map((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  return li;
+});
+
+// Dodaj wszystkie elementy li do listy ul#ingredients
+ingredientsList.append(...elements);
